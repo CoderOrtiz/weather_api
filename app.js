8caf7cf0a4bef1ee6b71417542cfc595e4e64a3e
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-06-22 20:52:15 -0400

    Added Image Icon with HTML Text to Send Info to The Browswer for the User

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
+// Creates an Express Application
 const express = require("express");
 const app = express();
-const https = require("http");
+// Requesting the https Module
+const https = require("https");
 
 app.get("/", function(req, res){
-
-    const url = "http://api.openweathermap.org/data/2.5/weather?q=Tampa,usa&units=imperial&appid=90cedd3136fdab5cc381234767da1b37";
+    // The URL Gets Us Live Data Using an API
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Tampa,usa&units=imperial&appid=90cedd3136fdab5cc381234767da1b37";
     
+    // Making an "https" Get Request
     https.get(url, function(response){
         console.log(response.statusCode);
 
+        // Taps into a Specific Moment, in this Instance, "data"
         response.on("data", function(data){
 
+            // "weatherData" Receives The "data" Parameter and Places the Data in a JSON Format
             const weatherData = JSON.parse(data);
+
+            // Fetching the Specific Items Wanted
             const temp = weatherData.main.temp;
             const weatherDiscription = weatherData.weather[0].description;
-            
+            const icon = weatherData.weather[0].icon;
+            const imageURL = `https://openweathermap.org/img/wn/${icon}@2x.png`
+
+            // Sending The Specific Items Requested Back to The Browser Using HTML 
+            res.write(`<h1>The temperature in Tampa is ${temp} degrees Fahrenheit, with ${weatherDiscription}.</h1>`);
+            res.write(`<img src="${imageURL}">`)
+            res.send();
+        });
     });
 });
 
-    res.send("Server is up and running.");
-});
-
+// app.listen() is Used to Bind & Listen To The Connections on a Specified Host & Port
 app.listen(3000, function(){
     console.log("The server is running on Port 3000");
 });
